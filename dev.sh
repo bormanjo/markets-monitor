5d1fec1ab2280d18889e9aab2a8913d453e57226
John-Craig Borman <borman.johncraig@gmail.com>
2019-06-21 13:54:15 -0400

    Removed old files

--- dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t py-markets-monitor .
-
-open http://localhost:8889
-
-docker run -it -p 8889:8889 -v $(pwd):/home/ --name=monitor_dev --rm py-markets-monitor bash /home/nb-start.sh
