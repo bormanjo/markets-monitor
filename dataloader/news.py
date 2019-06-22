dc86e5388e02d8cb757e0ccf9661c3c76324dca4
John-Craig Borman <borman.johncraig@gmail.com>
2019-06-22 12:17:04 -0400

    Added caching to rss feed component

--- dataloader/news.py
+++ dataloader/news.py
@@ -1,13 +1,75 @@
 import feedparser
+import datetime
+import pathlib
+import json
 
 rss_feeds = {
-    "WSJ Markets": "https://feeds.a.dj.com/rss/RSSMarketsMain.xml",
-    "WSJ World News": "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
-    "WSJ US Business": "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml"
+    "WSJ Markets": {
+        "link": "https://feeds.a.dj.com/rss/RSSMarketsMain.xml",
+        "updated": None
+    },
+    "WSJ World News": {
+        "link": "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
+        "updated": None
+    },
+    "WSJ US Business": {
+        "link": "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml",
+        "updated": None
+    }
 }
 
+CACHE_DIR = pathlib.Path("./dataloader/cache")
 
-def get_rss_feed(feed_key, top_n=None):
+
+def update_cache(feed_key):
+    """Updates the RSS cache"""
+
+    # Define cache file
+    cache_file = CACHE_DIR / (feed_key + ".json")
+
+    # Update
+    feed = feedparser.parse(rss_feeds[feed_key]['link'])
+    rss_feeds[feed_key]['updated'] = datetime.datetime.today()
+
+    # Cache data
+    with open(cache_file, "w") as file:
+        json.dump(feed.entries, file)
+
+    return feed.entries
+
+
+def get_cache(feed_key):
+    """Gets the cached data of the corresponding feed"""
+
+    cache_file = CACHE_DIR / (feed_key + ".json")
+
+    with open(cache_file, "r") as file:
+        entries = json.load(file)
+
+    return entries
+
+
+def get_rss_feed(feed_key):
+    """
+    Returns a parsed feed object
+    :param feed_key: A key in the rss_feeds dictionary identifying the feed
+    :return:
+    """
+
+    if rss_feeds[feed_key]['updated'] is None:
+        # Update Cache
+        entries = update_cache(feed_key)
+    elif (datetime.datetime.today() - rss_feeds[feed_key]['updated']).seconds > (60 * 5):
+        # Update Cache
+        entries = update_cache(feed_key)
+    else:
+        # Read Cache
+        entries = get_cache(feed_key)
+
+    return entries
+
+
+def get_rss_feed_markdown(feed_key, top_n=None):
     """
     Returns a RSS feed in markdown format
     :param feed_key: A key in the rss_feeds dictionary identifying the feed
@@ -15,7 +77,7 @@ def get_rss_feed(feed_key, top_n=None):
     """
 
     # Parse the rss feed
-    feed = feedparser.parse(rss_feeds[feed_key])
+    feed = get_rss_feed(feed_key)
 
     # Create the object output
     feed_str = ""
